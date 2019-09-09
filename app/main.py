c15870bb58a77fc21647088cd9a82c375565f70f
jodythai <thaibduong@gmail.com>
2019-09-09 16:45:07 +0700

    update

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 app.register_blueprint(homepage)
 app.register_blueprint(upload)
- 
-if __name__ == '__main__':
-  app.run(debug=True)
 
 app.debug = True
 
 # App Settings
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
+if __name__ == '__main__':
+  app.run(debug=True)
